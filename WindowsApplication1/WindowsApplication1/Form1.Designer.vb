6c05df6a1d63254137aa88820dd08b4fe75939c0
Yashantgyawali@gmail.com <YashantgyawalI@gmail.com>
2017-02-12 17:06:35 -0500

    The main project

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblPlayer2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblPlayer1 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblEasy = New System.Windows.Forms.Label()
+        Me.lblStart = New System.Windows.Forms.Label()
+        Me.lblScore2 = New System.Windows.Forms.Label()
+        Me.lblScore1 = New System.Windows.Forms.Label()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.RectangleShape2 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.SuspendLayout()
+        '
+        'lblPlayer2
+        '
+        Me.lblPlayer2.AutoSize = True
+        Me.lblPlayer2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblPlayer2.Font = New System.Drawing.Font("Showcard Gothic", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlayer2.Location = New System.Drawing.Point(506, 33)
+        Me.lblPlayer2.Name = "lblPlayer2"
+        Me.lblPlayer2.Size = New System.Drawing.Size(71, 26)
+        Me.lblPlayer2.TabIndex = 14
+        Me.lblPlayer2.Text = "Serve"
+        Me.lblPlayer2.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'lblPlayer1
+        '
+        Me.lblPlayer1.AutoSize = True
+        Me.lblPlayer1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblPlayer1.Font = New System.Drawing.Font("Showcard Gothic", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlayer1.Location = New System.Drawing.Point(31, 33)
+        Me.lblPlayer1.Name = "lblPlayer1"
+        Me.lblPlayer1.Size = New System.Drawing.Size(71, 26)
+        Me.lblPlayer1.TabIndex = 13
+        Me.lblPlayer1.Text = "Serve"
+        Me.lblPlayer1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label1.Font = New System.Drawing.Font("Showcard Gothic", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(316, 141)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 26)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Insane"
+        '
+        'lblEasy
+        '
+        Me.lblEasy.AutoSize = True
+        Me.lblEasy.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblEasy.Font = New System.Drawing.Font("Showcard Gothic", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEasy.Location = New System.Drawing.Point(219, 141)
+        Me.lblEasy.Name = "lblEasy"
+        Me.lblEasy.Size = New System.Drawing.Size(59, 26)
+        Me.lblEasy.TabIndex = 9
+        Me.lblEasy.Text = "Easy"
+        '
+        'lblStart
+        '
+        Me.lblStart.AutoSize = True
+        Me.lblStart.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblStart.Font = New System.Drawing.Font("Showcard Gothic", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStart.Location = New System.Drawing.Point(212, 74)
+        Me.lblStart.Name = "lblStart"
+        Me.lblStart.Size = New System.Drawing.Size(194, 67)
+        Me.lblStart.TabIndex = 8
+        Me.lblStart.Text = "START"
+        '
+        'lblScore2
+        '
+        Me.lblScore2.AutoSize = True
+        Me.lblScore2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblScore2.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore2.Location = New System.Drawing.Point(334, 0)
+        Me.lblScore2.Name = "lblScore2"
+        Me.lblScore2.Size = New System.Drawing.Size(18, 20)
+        Me.lblScore2.TabIndex = 12
+        Me.lblScore2.Text = "0"
+        '
+        'lblScore1
+        '
+        Me.lblScore1.AutoSize = True
+        Me.lblScore1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblScore1.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore1.Location = New System.Drawing.Point(245, 0)
+        Me.lblScore1.Name = "lblScore1"
+        Me.lblScore1.Size = New System.Drawing.Size(18, 20)
+        Me.lblScore1.TabIndex = 11
+        Me.lblScore1.Text = "0"
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.OvalShape1, Me.RectangleShape2, Me.RectangleShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(617, 246)
+        Me.ShapeContainer1.TabIndex = 15
+        Me.ShapeContainer1.TabStop = False
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.BackColor = System.Drawing.Color.Red
+        Me.OvalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.OvalShape1.Location = New System.Drawing.Point(583, 106)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.Size = New System.Drawing.Size(15, 17)
+        '
+        'RectangleShape2
+        '
+        Me.RectangleShape2.BackColor = System.Drawing.Color.Black
+        Me.RectangleShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.RectangleShape2.BorderColor = System.Drawing.SystemColors.ControlLightLight
+        Me.RectangleShape2.Location = New System.Drawing.Point(600, 91)
+        Me.RectangleShape2.Name = "RectangleShape2"
+        Me.RectangleShape2.Size = New System.Drawing.Size(12, 62)
+        '
+        'RectangleShape1
+        '
+        Me.RectangleShape1.BackColor = System.Drawing.Color.Black
+        Me.RectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.RectangleShape1.BorderColor = System.Drawing.SystemColors.ControlLightLight
+        Me.RectangleShape1.Location = New System.Drawing.Point(6, 94)
+        Me.RectangleShape1.Name = "RectangleShape1"
+        Me.RectangleShape1.Size = New System.Drawing.Size(12, 62)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(617, 246)
+        Me.Controls.Add(Me.lblPlayer2)
+        Me.Controls.Add(Me.lblPlayer1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblEasy)
+        Me.Controls.Add(Me.lblStart)
+        Me.Controls.Add(Me.lblScore2)
+        Me.Controls.Add(Me.lblScore1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Location = New System.Drawing.Point(6, 94)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPlayer2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents lblPlayer1 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblEasy As System.Windows.Forms.Label
+    Friend WithEvents lblStart As System.Windows.Forms.Label
+    Friend WithEvents lblScore2 As System.Windows.Forms.Label
+    Friend WithEvents lblScore1 As System.Windows.Forms.Label
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents RectangleShape1 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents RectangleShape2 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents OvalShape1 As Microsoft.VisualBasic.PowerPacks.OvalShape
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,383 @@
+﻿Public Class Form1
+    Dim moveRight As Boolean
+    Dim moveUp As Boolean
+    Dim wPress As Boolean
+    Dim sPress As Boolean
+    Dim iPress As Boolean
+    Dim kPress As Boolean
+    Dim Score1 As Integer
+    Dim Score2 As Integer
+    Dim count As Integer = 1
+    Dim difficulty As Integer = 5
+    Dim compRectangleDifficulty As Integer = 300
+    Dim changeServe As Boolean
+    Dim serve2 As Boolean
+    Dim serve1 As Boolean
+    Dim gameon As Boolean = False
+    Dim rectangleSpeed As Integer = 3
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        RectangleShape2Move()
+
+        If count Mod 2 = 0 Then
+            changeServe = True
+        Else
+            changeServe = False
+        End If
+
+        If wPress = True Then
+            RectangleShape1.Top -= rectangleSpeed
+        End If
+
+        If sPress = True Then
+            RectangleShape1.Top += rectangleSpeed
+        End If
+        If iPress = True Then
+            RectangleShape2.Top -= rectangleSpeed + 1
+        End If
+        If kPress = True Then
+            RectangleShape2.Top += rectangleSpeed + 1
+        End If
+        If moveRight = True Then
+            OvalShape1.Left += difficulty
+        Else
+            OvalShape1.Left -= difficulty
+        End If
+
+        If moveUp = True Then
+            OvalShape1.Top -= difficulty
+        Else
+            OvalShape1.Top += difficulty
+        End If
+
+        If OvalShape1.Left <= Me.ClientRectangle.Left Then
+            Timer1.Enabled = False
+            gameon = False
+            Score2 += 1
+            lblScore2.Text = Score2
+            count += 1
+            If Score1 < 5 And Score2 < 5 Then
+                If changeServe = True Then
+                    lblPlayer1.Visible = True
+                Else
+                    lblPlayer2.Visible = True
+                End If
+            Else
+                IfWin()
+
+            End If
+        End If
+
+
+        If OvalShape1.Left + OvalShape1.Width >= Me.ClientRectangle.Right Then
+            Timer1.Enabled = False
+            gameon = False
+            Score1 += 1
+            lblScore1.Text = Score1
+            count += 1
+            If Score1 < 5 And Score2 < 5 Then
+                If changeServe = True Then
+                    lblPlayer1.Visible = True
+                Else
+                    lblPlayer2.Visible = True
+                End If
+            Else
+                IfWin()
+            End If
+        End If
+
+
+
+        If OvalShape1.Left + OvalShape1.Width >= RectangleShape2.Left And OvalShape1.Top >= RectangleShape2.Top And OvalShape1.Top + OvalShape1.Height <= RectangleShape2.Top + RectangleShape2.Height Then
+            moveRight = False
+
+            'If OvalShape1.Top + OvalShape1.Height / 2 >= RectangleShape1.Top + RectangleShape1.Height / 2 Then
+            '    moveUp = True
+            'End If
+            'If OvalShape1.Top + OvalShape1.Height / 2 < RectangleShape1.Top + RectangleShape1.Height / 2 Then
+            '    moveUp = False
+            'End If
+            'If OvalShape1.Top + OvalShape1.Height / 2 >= RectangleShape2.Top + RectangleShape2.Height / 2 Then
+            '    moveUp = False
+            'End If
+            'If OvalShape1.Top + OvalShape1.Height / 2 < RectangleShape2.Top + RectangleShape2.Height / 2 Then
+            '    moveUp = True
+            'End If
+        End If
+
+        If OvalShape1.Left <= RectangleShape1.Left + RectangleShape1.Width And OvalShape1.Top >= RectangleShape1.Top And OvalShape1.Top + OvalShape1.Height <= RectangleShape1.Top + RectangleShape1.Height Then
+            moveRight = True
+            'If OvalShape1.Top + OvalShape1.Height / 2 >= RectangleShape1.Top + RectangleShape1.Height / 2 Then
+            '    moveUp = False
+            'End If
+            'If OvalShape1.Top + OvalShape1.Height / 2 < RectangleShape1.Top + RectangleShape1.Height / 2 Then
+            '    moveUp = True
+            'End If
+            'If OvalShape1.Top + OvalShape1.Height / 2 >= RectangleShape2.Top + RectangleShape2.Height / 2 Then
+            '    moveUp = False
+            'End If
+            'If OvalShape1.Top + OvalShape1.Height / 2 < RectangleShape2.Top + RectangleShape2.Height / 2 Then
+            '    moveUp = True
+            'End If
+        End If
+
+        If OvalShape1.Top <= Me.ClientRectangle.Top Then
+            moveUp = False
+
+        ElseIf OvalShape1.Top + OvalShape1.Height >= Me.ClientRectangle.Bottom Then
+            moveUp = True
+        Else
+
+        End If
+    End Sub
+
+
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        Dim moveUp1 As Boolean = True
+        Dim moveUp2 As Boolean = True
+
+        If RectangleShape1.Top <= Me.ClientRectangle.Top Then
+            moveUp1 = False
+            RectangleShape1.Top = Me.ClientRectangle.Top + 1
+        ElseIf RectangleShape1.Top + RectangleShape1.Height >= Me.ClientRectangle.Bottom Then
+            moveUp1 = False
+            RectangleShape1.Top = Me.ClientRectangle.Bottom - (1 + RectangleShape1.Height)
+            moveUp1 = True
+        End If
+        If RectangleShape2.Top <= Me.ClientRectangle.Top Then
+            moveUp2 = False
+            RectangleShape2.Top = Me.ClientRectangle.Top + 1
+        ElseIf RectangleShape2.Top + RectangleShape2.Height >= Me.ClientRectangle.Bottom Then
+            moveUp2 = False
+            RectangleShape2.Top = Me.ClientRectangle.Bottom - (1 + RectangleShape2.Height)
+            moveUp2 = True
+        End If
+
+        If e.KeyCode = Keys.W And moveUp1 <> False Then
+            wPress = True
+            sPress = False
+
+        End If
+        If e.KeyCode = Keys.S And moveUp1 <> False Then
+            sPress = True
+            wPress = False
+
+        End If
+
+        'If e.KeyCode = Keys.I Then
+        '    iPress = True
+        '    kPress = False
+
+        'End If
+        'If e.KeyCode = Keys.K Then
+        '    kPress = True
+        '    iPress = False
+
+        'End If
+        moveUp1 = True
+    End Sub
+
+    Private Sub lblStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblStart.Click
+        lblStart.Enabled = False
+        lblStart.Visible = False
+        lblEasy.Enabled = False
+        lblEasy.Visible = False
+        Label1.Visible = False
+        Label1.Enabled = False
+        lblPlayer1.Visible = True
+        OvalShape1.Top = 116
+        OvalShape1.Left = 19
+        wPress = False
+        sPress = False
+        iPress = False
+        kPress = False
+        RectangleShape1.Top = 94
+        RectangleShape2.Left = 6
+
+        RectangleShape2.Top = 84
+
+        RectangleShape2.Left = 600
+    End Sub
+
+    Private Sub Form1_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode = Keys.W Then
+            wPress = False
+        End If
+        If e.KeyCode = Keys.S Then
+            sPress = False
+        End If
+        'If e.KeyCode = Keys.I Then
+        '    iPress = False
+        'End If
+        'If e.KeyCode = Keys.K Then
+        '    kPress = False
+        'End If
+
+        If gameon = False Then
+            If e.KeyCode = Keys.L And lblPlayer2.Visible = True Then
+                serve2 = True
+                lblPlayer2.Visible = False
+                activateServe2()
+                gameon = True
+            End If
+            If e.KeyCode = Keys.D And lblPlayer1.Visible = True Then
+                serve1 = True
+                lblPlayer1.Visible = False
+                activateServe1()
+                gameon = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblEasy_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblEasy.Click
+        compRectangleDifficulty = 300
+        rectangleSpeed = 3
+        difficulty = 5
+        lblStart.Enabled = False
+        lblStart.Visible = False
+        lblEasy.Enabled = False
+        lblEasy.Visible = False
+        Label1.Visible = False
+        Label1.Enabled = False
+        lblPlayer1.Visible = True
+        OvalShape1.Top = 116
+        OvalShape1.Left = 19
+        wPress = False
+        sPress = False
+        iPress = False
+        kPress = False
+        RectangleShape1.Top = 94
+        RectangleShape2.Left = 6
+
+        RectangleShape2.Top = 84
+
+        RectangleShape2.Left = 600
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        compRectangleDifficulty = 300
+        rectangleSpeed = 4
+        difficulty = 8
+        lblStart.Enabled = False
+        lblStart.Visible = False
+        lblEasy.Enabled = False
+        lblEasy.Visible = False
+        Label1.Visible = False
+        Label1.Enabled = False
+        lblPlayer1.Visible = True
+        OvalShape1.Top = 116
+        OvalShape1.Left = 19
+        wPress = False
+        sPress = False
+        iPress = False
+        kPress = False
+        RectangleShape1.Top = 94
+        RectangleShape2.Left = 6
+
+        RectangleShape2.Top = 84
+
+        RectangleShape2.Left = 600
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblPlayer1.Click
+
+        lblPlayer1.Visible = False
+        activateServe1()
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblPlayer2.Click
+        lblPlayer2.Visible = False
+        activateServe2()
+    End Sub
+
+    Private Sub activateServe1()
+        Timer1.Enabled = True
+        OvalShape1.Top = 116
+        OvalShape1.Left = 19
+        wPress = False
+        sPress = False
+        iPress = False
+        kPress = False
+        RectangleShape1.Top = 94
+        RectangleShape2.Left = 6
+
+        RectangleShape2.Top = 84
+
+        RectangleShape2.Left = 600
+    End Sub
+
+    Private Sub activateServe2()
+        Timer1.Enabled = True
+        OvalShape1.Top = 106
+        OvalShape1.Left = 583
+        wPress = False
+        sPress = False
+        iPress = False
+        kPress = False
+        RectangleShape1.Top = 94
+        RectangleShape2.Left = 6
+
+        RectangleShape2.Top = 84
+
+        RectangleShape2.Left = 600
+    End Sub
+
+    Private Sub IfWin()
+        If Score1 = 5 Then
+            Timer1.Enabled = False
+            MessageBox.Show("Player 1 Wins")
+            lblStart.Enabled = True
+            lblStart.Visible = True
+            lblEasy.Enabled = True
+            lblEasy.Visible = True
+            Label1.Visible = True
+            Label1.Enabled = True
+            Score1 = 0
+            Score2 = 0
+            count = 1
+            lblScore1.Text = 0
+            lblScore2.Text = 0
+        ElseIf Score2 = 5 Then
+            Timer1.Enabled = False
+            MessageBox.Show("Player 2 Wins")
+            lblStart.Enabled = True
+            lblStart.Visible = True
+            lblEasy.Enabled = True
+            lblEasy.Visible = True
+            Label1.Visible = True
+            Label1.Enabled = True
+            lblScore1.Text = 0
+            lblScore2.Text = 0
+            Score1 = 0
+            Score2 = 0
+            count = 1
+        End If
+    End Sub
+
+    Private Sub RectangleShape2Move()
+
+        If RectangleShape2.Left - OvalShape1.Location.X <= 300 And (OvalShape1.Top < RectangleShape2.Top + 20 Or OvalShape1.Bottom > RectangleShape2.Bottom - 20) Then
+            If moveUp = True Then
+                iPress = True
+                kPress = False
+            Else
+                iPress = False
+                kPress = True
+            End If
+        Else
+            iPress = False
+            kPress = False
+        End If
+    End Sub
+
+
+End Class
